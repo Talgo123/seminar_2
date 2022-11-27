99c77f98a2382acb08f3d426b3dc17498d348ade
talgat <15talgat@mail.ru>
2022-11-27 13:23:37 +0300

    reshil_ zadachy_1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int Number = new Random(). Next (10, 99);
+Console.WriteLine(Number);
+
+int NumberA = Number / 10;
+int NumberB = Number % 10;
+int max = NumberB;
+
+if (NumberA > NumberB)  max = NumberA;
+
+Console.WriteLine($"Максимальная цифра из числа {Number} равна {max} ");
