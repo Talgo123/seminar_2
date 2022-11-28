4f267380a878618b6a7c59ecec1fc83ba28ea71b
talgat <15talgat@mail.ru>
2022-11-28 13:47:48 +0300

    add zadacha4

---
+++ zadacha_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+Console.WriteLine("Введите число: ");
+int Digit = int.Parse(Console.ReadLine());
+if (Digit % 7 == 0 && Digit % 23 == 0)
+{
+    Console.WriteLine($"{Digit} -> кратно");
+}
+else
+{ 
+    Console.WriteLine($"{Digit} -> некратно");
+}
